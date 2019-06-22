91531aa51606487c4eb0629c726e2223c1c850a5
Matthew Chan <33762231+ChingasChan@users.noreply.github.com>
2019-06-22 17:48:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return "For the demo, navigate to the endpoint /"
 
+@app.route('/user')
+def index2():
+	return "My personal changes"
+
 if __name__ == "__main__":
 	app.run(debug=True, use_reloader=True)
